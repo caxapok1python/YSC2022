f9d8ff01a501cd1533056779f594dd9975358c56
caxapok <caxapok.js@yandex.ru>
2022-11-29 10:45:50 +0300

    fix

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-from robot import Robot, Callback
-import cv2
 
-# robot = Robot('/dev/tty.usbmodem14201')  # with motors macbook
-robot = Robot('/dev/ttyUSB0')  # with motors rasbberry pi
-robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.5, k=1.0)
-# robot = Robot()  # without motors
-robot.setup_camera(0)
-robot.camera.set_line_params(dt=90, work_pos=robot.camera.height//2-100, work_width=300, work_height=20, blur=13)
-# _, img = robot.camera.cap.read()
-# img = cv2.imread('test1.png')
-# dt = robot.camera.autoconf_dt(img)
-# print(dt)
+while True:
+    a = input()
+    if (a):
+        break
+    from robot import Robot, Callback
+    import cv2
+    # robot = Robot('/dev/tty.usbmodem14201')  # with motors macbook
+    robot = Robot('/dev/ttyUSB0')  # with motors rasbberry pi
+    robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.5, k=1.0)
+    # robot = Robot()  # without motors
+    robot.setup_camera(0)
+    robot.camera.set_line_params(dt=90, work_pos=robot.camera.height//2-100, work_width=300, work_height=20, blur=13)
+    # _, img = robot.camera.cap.read()
+    # img = cv2.imread('test1.png')
+    # dt = robot.camera.autoconf_dt(img)
+    # print(dt)
 
-callback = Callback(robot)
-# robot.camera.track_line(print)
-robot.camera.track_line(callback.follow_line)
-robot = Robot('/dev/ttyUSB0')
-robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.5, k=1.0)
-robot.setup_camera(0)
-robot.stop()
-exit(0)
+    callback = Callback(robot)
+    # robot.camera.track_line(print)
+    robot.camera.track_line(callback.follow_line)
 
 
 
