aeda6e1c45527a3f9cbacce167d03c8b27e36a5e
caxapok <caxapok.js@yandex.ru>
2022-11-29 10:04:20 +0300

    one command startup

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ robot = Robot('/dev/tty.usbmodem14201')  # with motors
 robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.5, k=1.0)
 # robot = Robot()  # without motors
 robot.setup_camera(0)
-robot.camera.set_line_params(dt=-1, work_pos=410, work_width=300, work_height=20, blur=13)
+robot.camera.set_line_params(dt=90, work_pos=410, work_width=300, work_height=20, blur=13)
 # _, img = robot.camera.cap.read()
 # img = cv2.imread('test1.png')
 # dt = robot.camera.autoconf_dt(img)
