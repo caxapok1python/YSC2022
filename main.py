9bda862f0936a771ba539ec7ef1b20e68e8fecd7
caxapok <caxapok.js@yandex.ru>
2022-11-29 12:46:48 +0300

    fix

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ while True:
     import cv2
     # robot = Robot('/dev/tty.usbmodem14201')  # with motors macbook
     robot = Robot('/dev/ttyUSB0')  # with motors rasbberry pi
-    robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.5, k=1.0)
+    robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.3, k=3)
     # robot = Robot()  # without motors
     robot.setup_camera(0)
-    robot.camera.set_line_params(dt=90, work_pos=robot.camera.height//2-100, work_width=300, work_height=20, blur=13)
+    robot.camera.set_line_params(dt=90, work_pos=robot.camera.height//2+100 , work_width=300, work_height=20, blur=13)
     # _, img = robot.camera.cap.read()
     # img = cv2.imread('test1.png')
     # dt = robot.camera.autoconf_dt(img)
