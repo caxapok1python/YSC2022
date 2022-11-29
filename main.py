372cc9ae9f964a25597dcfc8f20940db30137dff
caxapok <caxapok.js@yandex.ru>
2022-11-29 13:05:51 +0300

    fix

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ while True:
     robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.3, k=3)
     # robot = Robot()  # without motors
     robot.setup_camera(0)
-    robot.camera.set_line_params(dt=80, work_pos=400, work_width=300, work_height=15, blur=13)
+    robot.camera.set_line_params(dt=110, work_pos=380, work_width=300, work_height=20, blur=13)
     # _, img = robot.camera.cap.read()
     # img = cv2.imread('test1.png')
     # dt = robot.camera.autoconf_dt(img)
     # print(dt)
 
     callback = Callback(robot)
-    # robot.camera.track_line(print)
-    robot.camera.track_line(callback.follow_line)
+    robot.camera.track_line(print)
+    # robot.camera.track_line(callback.follow_line)
 
 
 
