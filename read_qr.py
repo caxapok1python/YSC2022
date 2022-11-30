9bd9d1f5af5ef3cd26ac9be1757895021fa5be04
caxapok <caxapok.js@yandex.ru>
2022-11-30 19:40:56 +0300

    qr update

--- read_qr.py
+++ read_qr.py
@@ -10,7 +10,7 @@ while True:
     if not len(decodedObjects):
         continue
     data = decodedObjects[0].data
-    print(data.decode('utf-16-le'))
+    print(data.decode())
     break
 
 cap.release()
