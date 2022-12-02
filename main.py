4299d719ce110f9b0ada6a685c9e1742734c6514
caxapok <caxapok.js@yandex.ru>
2022-12-02 15:56:05 +0300

    main update

--- main.py
+++ main.py
@@ -4,23 +4,13 @@ from robot import Robot, Callback
 import cv2
 
 while True:
-    a = input()
-    if (a):
-        break
-
     # robot = Robot('/dev/tty.usbmodem14201')  # with motors macbook
     robot = Robot('/dev/ttyUSB0')  # with motors rasbberry pi
-    robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.3, k=4.0)
-    # robot = Robot()  # without motors
-    robot.setup_camera(0)
-    robot.camera.set_line_params(dt=40, work_pos=400, work_width=250, work_height=20, blur=13)
-    # _, img = robot.camera.cap.read()
-    # img = cv2.imread('test1.png')
-    # dt = robot.camera.autoconf_dt(img)
-    # print(dt)
 
+    robot.setup_motors((3, 7, 5), (2, 4, 6), max_power=0.3, k=4.0)
+    robot.setup_camera(0)
+    robot.camera.set_line_params(dt=int(sys.argv[1]), work_pos=400, work_width=250, work_height=20, blur=13)
     callback = Callback(robot)
-    # robot.camera.track_line(print)
     robot.camera.track_line(callback.follow_line)
 
 
